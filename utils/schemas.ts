b5ef320f8451c22efc637a9c9321b4df20ed2cac
Harish0705 <harish.balachandar@nzme.co.nz>
2024-11-09 12:42:51 +0530

    create validateWithZodSchema function to validate schema

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,9 +1,22 @@
 import * as z from 'zod';
-// import { ZodSchema } from 'zod';
+import { ZodSchema } from 'zod';
 
 export const profileSchema = z.object({
-  // firstName: z.string().max(5, { message: 'max length is 5' }),
-  firstName: z.string(),
+  firstName: z.string().min(3, { message: 'min length is 3' }),
   lastName: z.string(),
   username: z.string(),
 });
+
+// create a function and use it as we are using the same validation in multiple places
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  const result = schema.safeParse(data);
+  if (!result.success) {
+    const errors = result.error.errors.map((error) => error.message);
+
+    throw new Error(errors.join(', '));
+  }
+  return result.data;
+}
