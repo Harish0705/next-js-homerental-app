f9bacc5cd37fe38396725f1f9ad1e9c82552935c
Harish0705 <harish.balachandar@nzme.co.nz>
2024-11-09 12:43:43 +0530

    implement fetch and update profile

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,9 +2,10 @@
 
 import db from "./db";
 import { clerkClient, currentUser } from "@clerk/nextjs/server";
-// import { revalidatePath } from 'next/cache';
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { profileSchema } from "./schemas";
+import { profileSchema, validateWithZodSchema } from "./schemas";
+import { renderError } from "./error";
 
 export const createProfileAction = async (
   prevState: unknown,
@@ -15,7 +16,7 @@ export const createProfileAction = async (
     if (!user) throw new Error("Please login to create a profile");
 
     const rawData = Object.fromEntries(formData);
-    const validatedFields = profileSchema.parse(rawData);
+    const validatedFields = validateWithZodSchema(profileSchema, rawData);
 
     await db.profile.create({
       data: {
@@ -51,4 +52,48 @@ export const fetchProfileImage = async () => {
     },
   });
   return profile?.profileImage;
+};
+
+export const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logged in to access this route");
+  }
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) return redirect("/profile/create");
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: unknown,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+
+    const validatedFields = validateWithZodSchema(profileSchema, rawData);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields,
+    });
+    revalidatePath("/profile"); // revalidate to clear existing cache and refresh it
+    return { message: "Profile updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
