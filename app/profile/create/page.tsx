4f08f369737ebef59c296d81230f46bdef562d2a
Harish0705 <harish.balachandar@nzme.co.nz>
2024-11-09 12:30:46 +0530

    get user profile image

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,23 +1,28 @@
-import FormInput from '@/components/form/FormInput';
-import { SubmitButton } from '@/components/form/Buttons';
-import FormContainer from '@/components/form/FormContainer';
-import { createProfileAction } from '@/utils/actions';
+import FormInput from "@/components/form/FormInput";
+import { SubmitButton } from "@/components/form/Buttons";
+import FormContainer from "@/components/form/FormContainer";
+import { createProfileAction } from "@/utils/actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-function CreateProfile() {
+const CreateProfile = async () => {
+  const user = await currentUser();
+  // if the user already has profile redierect to home page
+  if (user?.privateMetadata?.hasProfile) redirect("/");
   return (
     <section>
-      <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
-      <div className='border p-8 rounded-md max-w-lg'>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
+      <div className="border p-8 rounded-md max-w-lg">
         <FormContainer action={createProfileAction}>
-          <div className='grid gap-4 mt-4 '>
-            <FormInput type='text' name='firstName' label='First Name' />
-            <FormInput type='text' name='lastName' label='Last Name' />
-            <FormInput type='text' name='username' label='Username' />
+          <div className="grid gap-4 mt-4 ">
+            <FormInput type="text" name="firstName" label="First Name" />
+            <FormInput type="text" name="lastName" label="Last Name" />
+            <FormInput type="text" name="username" label="Username" />
           </div>
-          <SubmitButton text='Create Profile' className='mt-8' />
+          <SubmitButton text="Create Profile" className="mt-8" />
         </FormContainer>
       </div>
     </section>
   );
-}
+};
 export default CreateProfile;
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,22 @@
 import { AiOutlineUser } from "react-icons/ai";
+import Image from "next/image";
+import { fetchProfileImage } from "@/utils/actions";
 
-function UserIcon() {
-  return <AiOutlineUser className='w-6 h-6 bg-primary rounded-full text-white' />;
-}
+const UserIcon = async () => {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage)
+    return (
+      <Image
+        src={profileImage}
+        alt="profile-image"
+        width={24}
+        height={24}
+        className="w-6 h-6 rounded-full object-cover"
+      />
+    );
+  return (
+    <AiOutlineUser className="w-6 h-6 bg-primary rounded-full text-white" />
+  );
+};
 export default UserIcon;
