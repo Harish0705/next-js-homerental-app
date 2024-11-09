45b9910343b6a8e593d4e57f053b5053b33fb648
Harish0705 <harish.balachandar@nzme.co.nz>
2024-11-09 14:47:46 +0530

    udpate config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["img.clerk.com","nrxxonjyzezpspiqdcxr.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nrxxonjyzezpspiqdcxr.supabase.co',
+      },
+    ],
   },
   experimental: {
     serverActions: {
