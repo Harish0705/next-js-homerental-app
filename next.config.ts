d4bee13c793abf7fbb0fb37390fba1ef8dc867cd
Harish0705 <harish.balachandar@nzme.co.nz>
2024-11-09 14:32:56 +0530

    add bodysizelimit in config file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["img.clerk.com"],
+    domains: ["img.clerk.com","nrxxonjyzezpspiqdcxr.supabase.co"],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
   },
 };
 
